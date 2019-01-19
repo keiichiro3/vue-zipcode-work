f97913c413a904a8f98e3f8028cffdcf835c8cb4
Keichiro Ohno <keichiro@MBP.local>
2019-01-20 04:12:16 +0900

    end

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <h1>ZIPCODE</h1>
+    <!-- <img src="./assets/logo.png"> -->
+    <form @submit.prevent="doSubmit">
+      <input v-model="zipcode" placeholder="郵便番号">
+      <button type="submit">送信</button>
+    </form>
+    <ul>
+      <li class="result" v-for="(item, index) in address" :key="index">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods: {
+    doSubmit: function() {
+      const jsonp = require('jsonp')
+      jsonp('http://zipcloud.ibsnet.co.jp/api/search?zipcode=' + this.zipcode, null, (err, data) => {
+        if (err) {
+          console.log(err)
+        } else {
+          var tmp = []
+          data.results.forEach(function(val, index, array) {
+            this.push(val.address1 + val.address2 + val.address3)
+          }, tmp)
+          this.address = tmp
+        }
+      })
+    }
+  },
+  data () {
+    return {
+      zipcode: '',
+      address: []
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  background-size: cover; 
+  background-image: url("./assets/rikka-wallpaper2.png")
+
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+li {
+  list-style: none;
+}
+
+.result {
+  font-size: 3rem;
+}
+</style>
